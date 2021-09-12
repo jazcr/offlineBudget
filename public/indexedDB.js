aa2e92c7ef2756618b980199a4094c3613b29244
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-09-12 14:31:14 -0600

    rewriting indexedDB and updates to service worker

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,44 +1,70 @@
-export function useIndexedDb(databaseName, storeName, method, object) {
-    return new Promise((resolve, reject) => {
-        const request = window.indexedDB.open(databaseName, 1);
-        let db,
-            tx,
-            store;
+let db;
 
-        request.onupgradeneeded = function (e) {
-            const db = request.result;
-            db.createObjectStore(storeName, { keyPath: "_id" });
-        };
+//indexed DB and version number
+const request = window.indexedDB.open("budgetTracker", 1);
 
-        request.onerror = function (e) {
-            console.log("There was an error");
-        };
-
-        request.onsuccess = function (e) {
-            db = request.result;
-            tx = db.transaction(storeName, "readwrite");
-            store = tx.objectStore(storeName);
-
-            db.onerror = function (e) {
-                console.log("error");
-            };
-            if (method === "put") {
-                store.put(object);
-            }
-            if (method === "clear") {
-                store.clear();
-            }
-            if (method === "get") {
-                const all = store.getAll();
-                all.onsuccess = function () {
-                    resolve(all.result);
-                };
-            }
-            tx.oncomplete = function () {
-                db.close();
-            };
-        };
-    });
+//getting db instance
+request.onupgradeneeded = ({ target }) => {
+    const db = target.result;
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore("fundsStore", { autoIncrement: true });
+    }
 };
 
+
+request.onsuccess = ({ target }) => {
+    db = target.result;
+    //will run the assessDB function to check if app is online
+    if (navigator.onLine) {
+        assessDB();
+    }
+};
+
+//console log request error
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+//
+const assessDB = () => {
+    let transaction = db.transaction(['fundsStore'], 'readwrite');
+    //allowing access to fundsStore
+    const store = transaction.objectStore('fundsStore');
+    //getting records from store
+    const getStore = store.getStore();
+
+    //this function will 'bulk add' transactions when app goes back online
+    getStore.onsuccess = function () {
+        if (getStore.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: 'POST',
+                body: JSON.stringify(getStore.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+            }).then((res) => res.json())
+                .then((res) => {
+                    if (res.length > 0) {
+                        transaction = db.transaction(['fundsStore', 'readwrite']);
+                        const currentStore = transaction.objectStore('fundsStore');
+                        //clearing any existing data in the store
+                        currentStore.clear();
+                        console.log('Existing store cleared and transactions are submitted.')
+                    }
+                })
+        }
+    };
+};
+
+const addToStore = (recordOfTrx) => {
+    console.log('Saving record of transaction.');
+    // creating a transaction on fundsStore database
+    const transaction = db.transaction(['fundsStore'], 'readwrite');
+    // creating access to fundsStore object
+    const store = transaction.objectStore('fundsStore');
+    store.add(recordOfTrx);
+};
+
+//event listener to fire when app goes online
 window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,93 @@
 //listener for static assets
-const STATIC_ASSETS = 'static-cache-v2';
+const STATIC_CACHE = 'static-cache-v2';
+const DATA_CACHE = "data-cache-v1";
+
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/indexedDB",
+    "/manifest.webmanifest",
+    "/service-worker.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512-512.png",
+];
+
+// install
+self.addEventListener("install", function (evt) {
+    // pre cache image data
+    evt.waitUntil(
+        caches.open(DATA_CACHE).then((cache) => cache.add("/api/images"))
+    );
+
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(STATIC_CACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== STATIC_CACHE && key !== DATA_CACHE
+        ) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE
+).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    //app will serve static assets if api is not requested 
+    evt.respondWith(
+        caches.open(STATIC_CACHE).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            })
+        })
+    );
+});
+
+
+
+
 
 
