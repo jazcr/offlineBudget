9553b46fa39899c6b88437594f061d932a4d9acb
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-09-13 15:52:51 -0600

    heroku connection

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budgetTracker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
-
+  process.env.MONGODB_URI || 'mongodb://localhost/budgetTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // routes
 app.use(require("./routes/api.js"));
 
